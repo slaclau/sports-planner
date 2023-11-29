287894fd17cdafdc126f6a1a6eee3b4b219681f5
slaclau <seb.laclau@gmail.com>
2023-11-29 16:24:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import garth
+import gi
+
+# Press the green button in the gutter to run the script.
+def main():
+    """Entrypoint"""
+
+
+if __name__ == '__main__':
+    print("Running main()")
+
